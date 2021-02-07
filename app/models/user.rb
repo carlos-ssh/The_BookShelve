9f68e94785e91d4186968546b0f472cb979793c6
Carlos-SSH <carlos.anezeh@me.com>
2021-02-06 22:43:21 -0600

    Create a navbar with admin rules for management and prototype for users.
    Add act as votable gem.
    
    Installed bootstrap icons for this project.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  acts_as_voter
+  
+  def username
+    email.split('@')[0].capitalize
+  end
 end
