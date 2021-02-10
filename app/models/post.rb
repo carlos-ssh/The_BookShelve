61f37ce5dc4210d462ac025ace43dc5390196c19
Carlos-SSH <carlos.anezeh@me.com>
2021-02-09 22:52:31 -0600

    Add style to views with bootstrap.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,10 @@
 class Post < ApplicationRecord
   has_and_belongs_to_many :categories
   has_one_attached :image
-  validates :image, content_type: [:png, :jpg, :jpeg]
+  has_rich_text :review
+  
   validates :title, :review, :image, presence: true
   acts_as_votable 
 
-  def resized_image
-    image.variant(resize: "240x135!")
-  end
+
 end
