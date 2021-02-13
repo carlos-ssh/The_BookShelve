b61a09488cb94cd6e669208632811a701a5b202d
Carlos-SSH <carlos.anezeh@me.com>
2021-02-13 16:55:37 -0600

    Updated bootsnap.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,4 +120,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.assets.compile = true
 end
