45a82384b905c118b393bda6bd14bf665b3eb7e8
Carlos-SSH <carlos.anezeh@me.com>
2021-02-16 19:19:03 -0600

    Update a line of code in aws settings.

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,5 @@
-Aws.config.update({
-  region: 'us-east-2',
-  credentials: Aws::Credentials.new(ENV['S3_KEY'], ENV['S3_SECRET']),
-})
-
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
+S3Client = Aws::S3::Client.new(
+  access_key_id: 'ACCESS_KEY_ID',
+  secret_access_key: 'SECRET_ACCESS_KEY',
+  region: 'REGION'
+) 
