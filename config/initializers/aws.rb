6c9d18cc781c9daced95b24fcf7f5c0c058aa9e2
Carlos-SSH <carlos.anezeh@me.com>
2021-02-16 19:00:19 -0600

    Updated bucket name issue.

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,4 +3,4 @@ Aws.config.update({
   credentials: Aws::Credentials.new(ENV['S3_KEY'], ENV['S3_SECRET']),
 })
 
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
+S3_BUCKET = Aws::S3::Resource.new.bucket('the-bookshelve')
