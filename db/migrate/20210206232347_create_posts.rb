f0e76eadf0fb99f89e806e0064f068ee263693d8
Carlos-SSH <carlos.anezeh@me.com>
2021-02-18 20:14:22 -0600

    Fixed issues with rating precision and scale.

--- db/migrate/20210206232347_create_posts.rb
+++ db/migrate/20210206232347_create_posts.rb
@@ -5,7 +5,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.string :title
       t.text :review
-      t.decimal :rating, scale: 1
+      t.decimal :rating, precision:1, scale: 5
 
       t.timestamps
     end
