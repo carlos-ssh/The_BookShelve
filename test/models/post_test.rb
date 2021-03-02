ed27c25281dad20a718afde6b656bae119517a70
Carlos-SSH <carlos.anezeh@me.com>
2021-03-01 21:11:57 -0600

    Fixed issues on categories sort_by

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    
+  test "should not save article without title" do
+      article = Article.new
+      assert_not article.save
+  end
+
 end
