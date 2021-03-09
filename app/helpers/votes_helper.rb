427c22e271faaad05597e7b3e627e511a3375c8f
Carlos-SSH <carlos.anezeh@me.com>
2021-03-09 14:40:38 -0600

    Add style to bookmark index page.

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,11 +1,11 @@
 module VotesHelper
   def vote_button(post)
     if !post.votes.where(user_id: current_user.id).exists?
-      link_to(image_tag('vote.png', height: '32', width: '23', class: 'vote'),
+      link_to(image_tag('vote.svg', height: '32', width: '23', class: 'vote'),
               votes_path(post_id: post.id,
                         category_id: @category.id), method: :post).html_safe
     else
-      link_to(image_tag('unvote.png', height: '32', width: '23', class: 'vote'),
+      link_to(image_tag('unvote.svg', height: '32', width: '23', class: 'vote'),
               vote_path(post_id: post.id,
                         category_id: @category.id), method: :delete).html_safe
     end
