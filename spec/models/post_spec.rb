b86dace4de7834de614c14efb3175a80caf3c96d
Carlos-SSH <carlos.anezeh@me.com>
2021-04-06 13:37:40 -0500

    Migrate dbs for tests.
    Add some test on spec/models.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,72 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  User.first_or_create!(email: 'carlos@example.com', password: '123123', password_confirmation: '123123')
-
-  it 'Has a title' do
-    post = Post.new(
-      title: '',
-      review: 'A valid Review',
-      rating: 5
+  let(:user) do
+    User.create(
+      email: 'hello@example.com',
+      created_at: '2021-04-06 22:35:32',
+      updated_at: '2021-04-06 22:35:32',
+      username: 'myname',
+      password: '123456',
+      password_confirmation: '123456'
     )
-    expect(post).to_not be_valid
-    post.title = 'A valid Title'
-    expect(post).to be_valid
+  end
+  let(:cat1) do
+    Category.create(
+      name: 'Reviews',
+      priority: 3
+    )
+  end
+  let(:cat2) do
+    Category.create(
+      name: 'Actions',
+      priority: 2
+    )
+  end
+  let(:subject) do
+    described_class.new(
+      title: 'This is a Test Title',
+      text: 'This is a long text, to test the post',
+      author_id: user.id,
+      categories_list: [cat1.id, cat2.id]
+    )
   end
 
-  it 'Has a Review' do
-    post = Post.new(
-      title: 'A valid Title',
-      review: '',
-      rating: 5
-    )
-    expect(post).to_not be_valid
-    post.review = 'A valid review'
-    expect(post).to be_valid
+  describe 'validations with subject' do
+    before(:each) do
+      subject.image.attach(io: File.open(Rails.root.join('spec', 'models', 'images', 'default.jpg')),
+                           filename: 'default.jpg', content_type: 'image/jpg')
+    end
+    it 'should not be valid without an image' do
+      subject.image = nil
+      expect(subject).to_not be_valid
+    end
+    it 'should be valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+    it 'should be invalid with small title' do
+      subject.title = 'Hello'
+      expect(subject).to_not be_valid
+    end
+    it 'should be invalid with small text' do
+      subject.text = 'Hello world!'
+      expect(subject).to_not be_valid
+    end
   end
 
-  it 'Has a Title at least 2 characters minimum' do
-    post = Post.new(
-      title: 'A valid Title',
-      review: '',
-      rating: 5
-    )
-    expect(post).to_not be_valid
-    post.title = '12'
-    expect(post).to be_valid
+  describe 'validations' do
+    it { should validate_presence_of(:image) }
+    it { should validate_length_of(:title) }
+    it { should validate_length_of(:text) }
   end
 
-  it 'Has a Review between 5..500 characters' do
-    post = Post.new(
-      title: 'A valid Title',
-      review: '12345',
-      rating: 5
-    )
-    expect(post).to_not be_valid
-    post.review = '123'
-    expect(post).to be_valid
-    fiveoo_string = 'adgohbeimrfxfsumgumqlmtpzxpohkhsal
-    yuhksjhbeimrfxfsumgumqkhsalyukyaqlmgljuadgohbeimrfx
-    fsumgumqlmtpzxpohkhsalyuhksjkyaqlmgljuadgolmtpzxpohk
-    hsalyuhksjkyaqlmgljuadgohbeimrfxfsumgumqlmtpzxpohkhs
-    alyuhksjkyaqlmgljuadgohbeimrfxfsumgumqlmtpzxpohkhsal
-    yuhksjkyaqlmgljukyaqlmgljuadgohbeimrfxfsumgumqlmtpzx
-    pohkhsalyuhksjkyaqlmgljuadgolmtpzxpohkhsalyuhksjkyaq
-    lmgljuadgohbeimrfxfsumgumqlmtpzxpohkhsalyuhksjkyaqlm
-    gljuadgohbeimrfxfsumgumqlmtpzxpohkhsalyuhksjkyaqlmgl
-    juwerwertwertwertsdfvbsdfgbdfgjhhjfghjertyerty12345'
-    post.review = fiveoo_string
-    expect(post).to be_valid
-    post.review = "#{fiveoo_string}1"
-    expect(post).to_not be_valid
-  end
-
-  it 'Has a numerical Rating' do
-    post = Post.new(
-      title: 'A valid Title',
-      review: 'A valid Review',
-      rating: 5
-    )
-    expect(post).to be_a(Integer)
+  describe 'associations' do
+    it { should belong_to(:author).class_name('User') }
+    it { should has_many(:votes) }
+    it { should has_many(:bookmarks) }
+    it { should has_and_belong_to_many(:categories) }
   end
 end
